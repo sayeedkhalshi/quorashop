7ea83e4d6020086e6d09795dcf42e4d89a86a62e
psayeed1990 <sayeedmondal1412@gmail.com>
2020-07-30 10:44:08 +0600

    password hash

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,21 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
+
 exports.signUp = catchAsync(async (req, res, next) => {
+  //check if email already exist
+  const oldUser = await User.findOne({ email: req.body.email });
+
+  if (oldUser) {
+    return res.status(400).json({ message: 'Email already exists' });
+  }
+
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
@@ -9,7 +23,9 @@ exports.signUp = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
+  const EmailVerifyToken = await createToken(newUser._id);
+
   res.status(200).json({
-    newUser,
+    EmailVerifyToken,
   });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -19,9 +20,10 @@ const userSchema = new Schema({
   },
   password: {
     type: String,
-    minlength: 12,
-    maxlength: 100,
+    minlength: [12, 'password must be minimum 12 characters long'],
+    maxlength: [100, 'password must be maximum 100 characters long'],
     required: [true, 'Password is required'],
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -33,6 +35,36 @@ const userSchema = new Schema({
       message: 'Password should be same',
     },
   },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+  },
+  photo: {
+    type: String,
+    default: 'default.jpg',
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
+});
+
+// userSchema.pre('save', async (next) => {
+//   if (!isModified('password') || this.isNew) return next();
+// })
 
 module.exports = User = mongoose.model('User', userSchema);
